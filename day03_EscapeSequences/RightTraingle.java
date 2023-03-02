e119101a40809ef7d7629bf40a637f59e36eb5e2
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-03-02 23:09:33 +0300

    Initial commit

---
+++ day03_EscapeSequences/RightTraingle.java
@@ -0,0 +1,14 @@
+package day03_EscapeSequences;
+
+public class RightTraingle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * * *");
+        System.out.println("* * * * * * *");
+    }
+
+}
