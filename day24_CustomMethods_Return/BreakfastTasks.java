6f37409c39d160fa7fb0559641d278811c7d1da9
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-04-11 03:06:47 +0300

    day24: Custom Methods - Return Methods

--- day24_CustomMethods_Return/BreakfastTasks.java
+++ day24_CustomMethods_Return/BreakfastTasks.java
@@ -2,8 +2,18 @@ package day24_CustomMethods_Return;
 
 public class BreakfastTasks {
     public static void main(String[] args) {
-        initials("cydeo", "School"); //W.S
 
+        initials("cydeo", "School"); //W.S
+        System.out.println("---------------------");
+
+        domain("Cydeo.School@amazon.com");
+        System.out.println("---------------------");
+
+        String[] emails = {"josh@gmail.com", "Jim@yahoo.com", "Elminur@cydeo.com", "Gulsen@gmail.com"};
+        for (String email : emails) {
+            domain(email);
+
+        }
 
 
     }
@@ -13,6 +23,12 @@ public class BreakfastTasks {
         String initial = firstName.charAt(0) + "." + lastName.charAt(0);
         initial = initial.toUpperCase();
         System.out.println("initial = " + initial);
+    }
+
+    //2. Create a method that can display the domain of the email.  domain(String email)
+    public static void domain(String email) {// Cydeo.School@gmail.com
+        String domain = email.substring(email.indexOf("@") + 1, email.lastIndexOf("."));
+        System.out.println("domain = " + domain);
     }
 
 }
