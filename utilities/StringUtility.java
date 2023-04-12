bffa7144efd3d3644fc5a2d7f15ef393f9169ec2
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-04-12 03:31:29 +0300

    day25: Custom Methods - Overloading

---
+++ utilities/StringUtility.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class StringUtility {
+    //print each character of the given string
+    public static void printEachChar(String str){
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+
+    }
+}
