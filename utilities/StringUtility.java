32762abb4bcda76217dd761934428b81c2c81237
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-04-12 03:45:08 +0300

    day25: Custom Methods - Overloading

--- utilities/StringUtility.java
+++ utilities/StringUtility.java
@@ -2,10 +2,22 @@ package utilities;
 
 public class StringUtility {
     //print each character of the given string
-    public static void printEachChar(String str){
+    public static void printEachChar(String str) {
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.charAt(i));
         }
 
+    }
+
+    // reverses the given string and returns the reversed string
+    public static String reverse(String str) {// "Java"
+        String result = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+
+        }
+
+        return result; // "avaJ"
     }
 }
