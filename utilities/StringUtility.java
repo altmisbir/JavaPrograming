9b16a8a4eaae6746a16afa564681048e0e1be0c4
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-04-12 04:07:06 +0300

    day25: Custom Methods - Overloading

--- utilities/StringUtility.java
+++ utilities/StringUtility.java
@@ -24,7 +24,7 @@ public class StringUtility {
     }
 
     //checks if the given String is palindrome, returns boolean
-    public static boolean isPalindrome(String str){
+    public static boolean isPalindrome(String str) {
         return reverse(str).equalsIgnoreCase(str);
     }
 
@@ -37,5 +37,20 @@ public class StringUtility {
         Arrays.sort(ch2);
 
         return Arrays.equals(ch1, ch2);
+    }
+
+    //removes the duplicates from given string, returns String.  "aaaaaaabbbbbccccc" ====> "abc"
+    public static String removeDuplicates(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);//aaaaabbbbcccc
+
+            if (!result.contains("" + each)) {
+                result += each;
+            }
+        }
+        return result;
     }
 }
