3fc398b1c59f9dc83a511db1f213e1413f0e6a21
altmisbir <altmisbir@msn.com>
2023-05-07 00:07:05 +0300

    day35 : Encapsulation

---
+++ day35_Encapsulation/encapsulation/Employee.java
@@ -0,0 +1,58 @@
+package day35_Encapsulation.encapsulation;
+
+public class Employee {
+    private String name;
+    private char gender;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            return;
+        }
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if (!(gender == 'M' || gender == 'F')) {
+            return;
+        }
+
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 16 || age > 90) {
+            return;
+        }
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+
+
+        this.salary = salary;
+    }
+
+
+
+
+
+
+}
