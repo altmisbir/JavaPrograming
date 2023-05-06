509b7a38f54f19115fae28f2561c1760cd097a30
altmisbir <altmisbir@msn.com>
2023-05-07 00:54:42 +0300

    day36 : Inheritance Intro

---
+++ day36_Inheritance/animalTask/Dog.java
@@ -0,0 +1,13 @@
+package day36_Inheritance.animalTask;
+
+public class Dog extends Animal {
+
+    public void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+/*
+    dog:
+        6 variables
+        7 methods
+ */
